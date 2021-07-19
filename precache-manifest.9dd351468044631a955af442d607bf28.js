3e1bc13c0e47ae9f5cba9b05ef54f10911788cf9
Denis V. Chumakov <den@10-13.ru>
2021-07-19 13:58:07 +0700

    deploy

---
+++ precache-manifest.9dd351468044631a955af442d607bf28.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c29b894230998258d15f3ddf109517",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "b03ad4fd5bd15d7f7867",
+    "url": "/css/app.833b674d.css"
+  },
+  {
+    "revision": "dcfba20b1cf63cca4938",
+    "url": "/css/chunk-vendors.1a21b110.css"
+  },
+  {
+    "revision": "4e7df8616bc6a9f4a191ef50a425ee97",
+    "url": "/img/coins/RusCoin.svg"
+  },
+  {
+    "revision": "e8ebfbffb6ab2cf3fff1ccff01f91c96",
+    "url": "/img/coins/RusCoinBack.svg"
+  },
+  {
+    "revision": "79dcd4e07593611c7dbdaaab02100077",
+    "url": "/img/coins/logo-exchange.svg"
+  },
+  {
+    "revision": "503b45695112550de4af6971e6db4481",
+    "url": "/img/coins/relictum.svg"
+  },
+  {
+    "revision": "5f446146e2483742eb890d3717bbfe7b",
+    "url": "/img/coins/uniswap.svg"
+  },
+  {
+    "revision": "b7c54a63043d37734e7c3d37fed7edbb",
+    "url": "/img/coins/waves.svg"
+  },
+  {
+    "revision": "8c40cf7044f529cad7b4d10eb55bee36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b03ad4fd5bd15d7f7867",
+    "url": "/js/app.032abef3.js"
+  },
+  {
+    "revision": "dcfba20b1cf63cca4938",
+    "url": "/js/chunk-vendors.bf5a4047.js"
+  },
+  {
+    "revision": "08489a9cfef0d6b8c3ede97cd7be061e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
